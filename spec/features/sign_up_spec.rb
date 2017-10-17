320689a2f569f818039a7c0ff53a62bd6058c54e
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2017-10-17 16:35:56 -0400

    we made user sign up with devise gem and added Rspec tests

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+
+feature  'i want to be able to sign up' do
+
+
+
+ scenario "user clicks on the submit button" do
+   visit new_user_registration_path
+   click_button 'Sign up'
+    expect(page).to have_content("State can't be blank")
+    expect(page).to have_content("Username can't be blank")
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+ end
+ end
