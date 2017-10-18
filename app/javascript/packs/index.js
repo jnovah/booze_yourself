273a8611ef59b1faf921bf9ef0e21d983d2eff7d
uncommonAP <aaron.paul_r@outlook.com>
2017-10-18 13:26:12 -0400

    removal of conflict text

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import BoozeYourself from '../react/src/BoozeYourself';
-=======
-import BoozeYourself from '../react/src/boozeYourself';
->>>>>>> aa5a89f304da0496fd3d6aca928c3b191f144ec9
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<BoozeYourself />, document.getElementById('app'));
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-  root "static_files#index"
-=======
   root "static_files#index"
 
   get '*path', to: 'static_files#index'
->>>>>>> aa5a89f304da0496fd3d6aca928c3b191f144ec9
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
