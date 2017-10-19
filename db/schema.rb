944d8ea53f3fac33253818b6190f438ac1438d2f
User <max@Users-MacBook-Air.local>
2017-10-19 11:50:14 -0400

    config routes ordering

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171018154249) do
-
+ActiveRecord::Schema.define(version: 20171018184616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +39,7 @@ ActiveRecord::Schema.define(version: 20171018154249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
