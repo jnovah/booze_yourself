052f1bc8f70fc186394f858d89df7b70c116c7ed
User <max@Users-MacBook-Air.local>
2017-10-19 09:45:05 -0400

    nested the scenarios and made the scenario descriptions clearer

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature  'i want to be able to sign up' do
-  scenario "user clicks on the submit button" do
+  scenario "user successfully signs up" do
     visit new_user_registration_path
     fill_in "Username", with: "me"
     fill_in "State", with: "NH"
@@ -11,12 +11,9 @@ feature  'i want to be able to sign up' do
     click_button 'Sign up'
       expect(page).to have_content("Welcome! You have signed up successfully")
   end
-end
-
-feature  'i want to be able to sign up' do
- scenario "user clicks on the submit button" do
-   visit new_user_registration_path
-   click_button 'Sign up'
+  scenario "user just clicks on the submit button" do
+    visit new_user_registration_path
+    click_button 'Sign up'
     expect(page).to have_content("State can't be blank")
     expect(page).to have_content("Username can't be blank")
     expect(page).to have_content("Email can't be blank")
