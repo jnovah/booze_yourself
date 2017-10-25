23a37deced900f6df8c559925535e4b8264f1585
User <max@Users-MacBook-Air.local>
2017-10-25 17:45:41 -0400

    messed around with button toggles

--- app/javascript/react/src/Components/NewestBeersTile.js
+++ app/javascript/react/src/Components/NewestBeersTile.js
@@ -31,6 +31,13 @@ const NewestBeersTile = props => {
           {beers}
           {beers}
           {beers}
+          {beers}
+          {beers}
+          {beers}
+          {beers}
+          {beers}
+          {beers}
+          {beers}
         </tbody>
       </table>
     </div>
--- app/javascript/react/src/containers/Home.js
+++ app/javascript/react/src/containers/Home.js
@@ -13,12 +13,45 @@ class Home extends Component {
       brewery: "Busch",
       rating: 2, id: 1}, {beerName: "Bud",
       brewery: "Busch",
-      rating: 2, id: 2}]
+      rating: 2, id: 2}],
+      buttonToggle: false,
+      secondButtonToggle: false
     }
+    this.handleButtonToggle=this.handleButtonToggle.bind(this)
+    this.handleSecondButtonToggle=this.handleSecondButtonToggle.bind(this)
 
 
   }
+
+
+  handleButtonToggle(){
+    if(this.state.buttonToggle){
+      this.setState({ buttonToggle: false})
+    } else {
+      //fetch
+      this.setState({ buttonToggle: true })
+    }
+  }
+
+   handleSecondButtonToggle(){
+     if(this.state.secondButtonToggle){
+       this.setState({ secondButtonToggle: false })
+     } else {
+       //fetch
+       this.setState({ secondButtonToggle: true })
+     }
+   }
+
+
   render(){
+  let clicked=''
+  if (this.state.buttonToggle){
+    clicked='clicked'
+  }
+ let secondClicked =''
+  if(this.state.secondButtonToggle){
+    secondClicked='clicked'
+  }
 
     return(
 
@@ -26,9 +59,12 @@ class Home extends Component {
        <div className='header-image' >
         <div className="small-4 small-offset-4 cell">
           <input className='search-bar' placeholder='Search All Beers'></input>
-          <div className="search-button-wrapper">
-            <button className='small-2 small-offset-5 search-button'>Search</button>
-          </div>
+          <span className='search-button-wrapper'>
+            <button onClick={this.handleButtonToggle} className={`${clicked} small-5 small-offset-1 search-button top-twenty`}>Top Twenty Beers</button>
+          </span>
+          <span className="search-button-wrapper">
+            <button onClick={this.handleSecondButtonToggle} className={`${secondClicked} small-5 search-button`}>Newest Beers</button>
+          </span>
         </div>
        </div>
         <div>
